323e475da0c494d990e8d328eee4bc437e2615df
hmallafi <mallafi.hadary@gmail.com>
2018-04-21 09:59:03 +0700

    bikin koneksi mysql
    koneksi mysql

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-echo 'Belajar microservices';
+$servername = "mysql://mysql:3306/";
+$username = "soa";
+$password = "soa2018";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
 ?>
